4fb185be2ce98d7059edfdc3bfe293fdc2ec7890
Terry-Kinegeni <88835562+Terry-Kinegeni@users.noreply.github.com>
2023-06-23 08:00:07 +0300

    Add files via upload

---
+++ app/Views/passwordchanged.php
@@ -0,0 +1,40 @@
+<?php require_once "controllerUserData.php"; ?>
+<?php
+if($_SESSION['info'] == false){
+    header('Location: login.php');  
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="registerlogin/fonts/material-icon/css/material-design-iconic-font.min.css">
+	<link rel="stylesheet" href="registerlogin/css/style.css">
+	<title>Login Form</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 form login-form">
+            <?php 
+            if(isset($_SESSION['info'])){
+                ?>
+                <div class="alert alert-success text-center">
+                <?php echo $_SESSION['info']; ?>
+                </div>
+                <?php
+            }
+            ?>
+                <form action="login.php" method="POST">
+                    <div class="form-group">
+                        <input class="form-control button" type="submit" name="login-now" value="Login Now">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
